137581be131adb525fd6e1ff7c57c1dcc14533be
Vincent Guenat <vincent.guenat@orange.fr>
2015-05-19 18:34:07 +0200

    Création du fichier de fonctions PHP-BDD
    Toutes les fonctions liant le php et la bdd.

---
+++ Sources/bdd.php
@@ -0,0 +1,35 @@
+<?php
+	
+	function connectBDD() {
+		// Infos de connexion
+		$db = "postgres";
+		$user = "postgres";
+		$pass = "postgres";
+
+		// Connexion
+		$connect = pg_connect("dbname=$db user=$user password=$pass");
+		if ($connect)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	function requeteBDD($query) {
+		if (connectBDD())
+		{
+			$username = $_POST['login'];
+			$ajoute = false;
+			$result = pg_exec($connect,$query);
+			return $result;
+		}
+		else
+		{
+			echo "Erreur de connexion.";
+		}
+	}
+
+?>
