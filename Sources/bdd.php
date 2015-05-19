102a95745811de116b8e53ed93548ca5769c03f2
Vincent Guenat <vincent.guenat@orange.fr>
2015-05-19 19:02:03 +0200

    Suppression fonction insertion

--- Sources/bdd.php
+++ Sources/bdd.php
@@ -29,24 +29,4 @@
 		}
 	}
 	
-	function insertUser($u) {
-		requeteBDD("INSERT INTO utilisateur VALUES ($u[0],$u[1],$u[2],$u[3],$u[4],$u[5],$u[6],$u[7],$u[8],$u[9],$u[10],$u[11])");
-	}
-
-	function insertObj($objet) {
-		requeteBDD("INSERT INTO objet VALUES ($objet[0],$objet[1],$objet[2],$objet[3],$objet[4],$objet[5],$objet[6],$objet[7],$objet[8],$objet[9],$objet[10],$objet[11],$objet[12])");
-	}
-	
-	function insertEnch($e) {
-		requeteBDD("INSERT INTO encherir VALUES ($e[0],$e[1],$e[2],$e[3])");
-	}
-	
-	function insertCat($cat) {
-		requeteBDD("INSERT INTO categorie VALUES ($cat[0],$cat[1])");
-	}
-	
-	function insertNote($n) {
-		requeteBDD("INSERT INTO note VALUES ($n[0],$n[1],$n[2],$n[3],$n[4],$n[5],$n[6])");
-	}
-	
 ?>
