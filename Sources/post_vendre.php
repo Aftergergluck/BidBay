f758ada76d38f98d9eb1891291df6444d36f4590
ManuAA <manu.aacha@hotmail.fr>
2015-05-23 13:49:54 +0000

    deleted:    Sources/gestionFormAjoutObj.php 	modified:   Sources/post_vendre.php 	modified:   Sources/vendre.php

--- Sources/post_vendre.php
+++ Sources/post_vendre.php
@@ -56,7 +56,32 @@
         );";                      
          
          
-  $resultat = pg_query($insert);      
+  pg_query($insert);      
   echo "Requete effectuee";     
+
+
+
+//Traitement de l'image  
+    if (isset($_FILES['image']) AND $_FILES['image']['error'] == 0)
+      {
+        // Test de la taille du fichier < 3Mo
+        if ($_FILES['image']['size'] <= 3000000)
+        {
+                // Test extension
+                $infosfichier = pathinfo($_FILES['image']['name']);
+                $extension_upload = $infosfichier['extension'];
+                $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+                if (in_array($extension_upload, $extensions_autorisees))
+                {
+                        // Validation et stockage
+                        // Sous le nom objeti  i étant l'idobjet
+                        move_uploaded_file($_FILES['image']['tmp_name'], 'uploads\photoobjet\objet' .$idobj);
+                        echo "L'envoi a bien été effectué !";
+                }
+        }
+      }
+
+
+  
   footerHTML();
 ?>
