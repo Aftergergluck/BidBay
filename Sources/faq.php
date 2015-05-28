b4eb0920a301309b8fba4b09cd255abe076771c3
cbanquet <charles.banquet@live.com>
2015-05-28 09:56:52 +0200

    Ajout de la page FaQ

--- Sources/faq.php
+++ Sources/faq.php
@@ -4,10 +4,30 @@
 ?>
 
 <h1 class="Tpage">Foire Aux Questions</h1>
-	<br><br><br><br><br><br><br><br>
-	<p>Voici la page d'FAQ/Aides</p>
-	<br><br><br><br><br><br><br><br>
-
+	<h3><p>Voici la page d'FAQ/Aides</p></h3>
+		<b><p>Ici se trouve plusieurs questions, auquel nous répondons, que peut se poser un utilisateur lorsqu'il est sur BidBay</p></b>
+		<br></br>
+		<b><p>Est-ce qu'on a le droit d'enchérir sur notre propre enchère?</p></b>
+		<p>Rien de stipule le contraire, mais enchérir sur sa propre enchère ferait fuir vos acheteurs potentiel.
+		De plus si vous gagnez l'enchère vous vous devrez des sous et vous n'aurez pas vendu votre objet.</p>
+		<br></br>
+		<b><p>Si je ne suis pas connecté est-ce que j'ai le droit de voir des profils et des objets?</p></b>
+		<p>Bien sur, rien n'empêche de voir des profils ou des objets si vous n'êtes pas connecté</p>
+		<br></br>
+		<b><p>Est-ce qu'il est possible de retirer la mise qu'on vient de faire sur une enchère?</p></b>
+		<p>Non, il n'est pas possible de faire ça, une fois qu'on a enchérie sur un objet on ne peut se retirer
+		il faut attendre ou espérer que quelqu'un d'autre mise dessus si on ne veut pas de cet objet</p>
+		<br></br>
+		<b><p>Est-il possible changer le pas de mon enchère?</p></b>
+		<p>Non il n'est pas possible de modifier le pas une fois l'objet mis en vente</p>
+		<br></br>
+		<b><p>Est-il possible de changer la mise de départ de mon enchère?</p></b>
+		<p>Non il n'est pas possible la mise de départ, surtout après qu'une personne a misé dessus</p>
+		<br></br>
+		<b><p>Est-ce que je peux modifier la date de mise de mon objet?</p></b>
+		<p>Non on ne peut pas modifier la date de mise de son objet de même que la date de fin non plus</p>
+		<br></br>
+		
 <?php
 	//require 'headfoot.php';
 	footerHTML();
