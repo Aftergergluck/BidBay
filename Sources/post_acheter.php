57b7b6ecee4e57943afdec324c1d2c5199d73c4c
cbanquet <charles.banquet@live.com>
2015-05-28 18:07:35 +0200

    Ajout de la mise d'un objet

---
+++ Sources/post_acheter.php
@@ -0,0 +1,42 @@
+<?php
+	require 'head_foot.php';
+	headerHTML();
+	session_start();
+	$nomobj = "Pomme";
+	$login = $_SESSION['login'];
+	$pas = $_SESSION['pasobj'];
+	$ancienprix = $_SESSION['prix'];
+?>
+
+<?php
+		/*on récupère les infos que l'utilisateur a écrit dans les champs login et mot de passe*/
+		$mise = isset($_POST['miser']) ? $_POST['miser'] : '';
+		/*on vérifie si l'utilisateur est connecté*/
+		if($login== ''){
+			header('Location: acheter.php?error=2');
+		}
+		/*on vérifie si l'utilisateur a saisi un prix*/
+		elseif($mise ==''){
+			header ('Location: acheter.php?error=3');
+		}
+		/*on vérifie si l'utilisateur a saisi un prix supérieur au pas*/
+		elseif($mise < $pas){
+			header ('Location: acheter.php?error=1');
+		}
+		else{
+			$nouveauprix = $mise + $ancienprix;
+			/*connexion à la base de donnée*/
+			$db = "postgres";
+			$user = "postgres";
+			$pass = "postgres";
+			$connect = pg_connect("dbname=$db user=$user password=$pass");
+			/*on change dans la base de données on met à jour le prix et le dernier utilisateur qui a miser sur l'objet*/
+			pg_exec($connect,"UPDATE objet SET mailacheteur='$login', prixinit='$nouveauprix' WHERE nomobj='$nomobj'");
+			header('Location: objet.php');
+		}
+?>
+
+<?php
+	//require 'headfoot.php';
+	footerHTML();
+?>
